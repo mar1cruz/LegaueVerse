6e39267818b0026c7c3be02328efa6e1439ab62f
dmitry <ddovidovich@list.ru>
2024-05-28 15:31:12 +0300

    first commit

---
+++ src/pages/Nhl/Nhl.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Dota = () => {
+    return (
+        <div>
+            Dota
+        </div>
+    );
+};
+
---
+++ src/router/router.tsx
@@ -0,0 +1,11 @@
+import {createBrowserRouter} from "react-router-dom";
+import { Navigate } from "react-router-dom"
+
+export const router = createBrowserRouter([
+    {
+        path:'/',
+        // index:true,
+        element:<Navigate
+    }
+])
+
